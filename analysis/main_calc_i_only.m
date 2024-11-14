a82288bb1da7d980fc04f2c711db2a3c2a884d7f
Ian <iwillows@asu.edu>
2024-11-14 13:50:11 -0700

    Add Ian's "main" file that he used to run calc_i.m in the very beginning to the analysis folder.

---
+++ analysis/main_calc_i_only.m
@@ -0,0 +1,16 @@
+clear all; 
+close all; 
+clc;
+
+%inputs
+f = 400e3;
+Va = [800:100:1200];
+Vb = 1e3;
+N = 1; 
+phi = pi/6;
+%phi = [pi/12:pi/12:pi/3];
+L = 100e-6;
+T = 1./f;
+
+[i_pk,i_rms,isw_prim14_pk,isw_prim23_pk,isw_prim14_rms,isw_prim23_rms,...
+    isw_sec14_pk,isw_sec23_pk,isw_sec14_rms,isw_sec23_rms] = calc_i(f,Va,Vb,N,phi,L,T)
