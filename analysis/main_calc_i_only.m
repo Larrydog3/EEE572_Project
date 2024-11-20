584a140ae4abbc5bdd7502e00ed159f216e43d1e
Nick James Kirkby <20824939+driftregion@users.noreply.github.com>
2024-11-20 14:06:53 -0700

    add first pass at vectorized code: works for single inputs, mesh not yet tested

--- analysis/main_calc_i_only.m
+++ analysis/main_calc_i_only.m
@@ -13,4 +13,4 @@ L = 100e-6;
 T = 1./f;
 
 [i_pk,i_rms,isw_prim14_pk,isw_prim23_pk,isw_prim14_rms,isw_prim23_rms,...
-    isw_sec14_pk,isw_sec23_pk,isw_sec14_rms,isw_sec23_rms] = calc_i(f,Va,Vb,N,phi,L,T)
+    isw_sec14_pk,isw_sec23_pk,isw_sec14_rms,isw_sec23_rms] = calc_i(f,Va,Vb,N,phi,L)
